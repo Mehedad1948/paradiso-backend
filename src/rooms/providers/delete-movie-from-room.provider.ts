1be51be0c345476cc10b72254350d7d597ce1cad
Mehrdad <mnourib13@gmail.com>
2025-06-11 21:44:34 +0330

    feat: enhance delete movie from room functionality with user role validation and improved error handling

--- src/rooms/providers/delete-movie-from-room.provider.ts
+++ src/rooms/providers/delete-movie-from-room.provider.ts
@@ -10,10 +10,14 @@ import { MoviesService } from 'src/movies/providers/movies.service';
 import { RatingsService } from 'src/ratings/providers/ratings.service';
 import { Repository } from 'typeorm';
 import { Room } from '../room.entity';
+import { REQUEST } from '@nestjs/core';
+import { Request } from 'express';
+import { REQUEST_USER_KEY } from 'src/auth/constants/auth.constants';
 
 @Injectable()
 export class DeleteMovieFromRoomProvider {
   constructor(
+    @Inject(REQUEST) private readonly request: Request,
     private readonly movieService: MoviesService,
 
     @Inject(forwardRef(() => RatingsService))
@@ -24,25 +28,28 @@ export class DeleteMovieFromRoomProvider {
   ) {}
 
   async delete(roomId: number, movieId: string) {
+    const userPayload = this.request[REQUEST_USER_KEY];
+    const userId = userPayload.id;
+    const role = userPayload.role;
+
     const room = await this.roomRepository.findOne({
       where: { id: roomId },
-      relations: ['movies'],
+      relations: ['movies', 'movies.addedBy'],
     });
 
     if (!room) {
       throw new NotFoundException('Room was not found.');
     }
 
-    const movie = await this.movieService.getMovieById(movieId);
+    const movie = room.movies.find((m) => m.id === movieId);
     if (!movie) {
-      throw new NotFoundException('Movie was not found.');
+      throw new ConflictException('Movie is not in the room.');
     }
 
-    const isMovieInRoom = room.movies.some(
-      (roomMovie) => roomMovie.id === movieId,
-    );
-    if (!isMovieInRoom) {
-      throw new ConflictException('Movie is not in the room.');
+    if (role !== 'admin' && movie.addedBy.id !== userId) {
+      throw new ConflictException(
+        'You do not have permission to remove this movie.',
+      );
     }
 
     await this.ratingsService.deleteRatingWithMovieAndRoom(roomId, movieId);
