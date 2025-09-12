51fb76972ac61cc27e3d194ca1848de12679aea6
Mehrdad <mnourib13@gmail.com>
2025-09-12 20:46:34 +0330

    Improt providers in links module

--- src/room-invite-links/room-invite-links.module.ts
+++ src/room-invite-links/room-invite-links.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { RoomInviteLinksService } from './room-invite-links.service';
 import { RoomInviteLinksController } from './room-invite-links.controller';
+import { UpdateRoomInviteLinkProvider } from './providers/update-room-invite-link.provider';
+import { DeleteRoomInviteLinkProvider } from './providers/delete-room-invite-link.provider';
+import { VerifyRoomInviteLinkProvider } from './providers/verify-room-invite-link.provider';
+import { CreateRoomInviteLinkProvider } from './providers/create-room-invite-link.provider';
+import { GetRoomInviteLinkProvider } from './providers/get-room-invite-link.provider';
 
 @Module({
   controllers: [RoomInviteLinksController],
-  providers: [RoomInviteLinksService],
+  providers: [
+    RoomInviteLinksService,
+    UpdateRoomInviteLinkProvider,
+    DeleteRoomInviteLinkProvider,
+    VerifyRoomInviteLinkProvider,
+    CreateRoomInviteLinkProvider,
+    GetRoomInviteLinkProvider,
+  ],
 })
 export class RoomInviteLinksModule {}
